412da0d4a4cb914fc26f51aa94d679dea2560d83
Borey <52574681+BoreyJr@users.noreply.github.com>
2021-06-12 11:29:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const qoutes = [ 
-    'Imaginary is more than knowledge - Albert Estien',
+    'Imagination is more than knowledge - Albert Estien',
     'The Best Way To Get Started Is To Quit Talking And Begin Doing. – Walt Disney',
     'JavaScript is the world is most misunderstood programming language. - Js lover',
     'If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You - Steve Jobs',
